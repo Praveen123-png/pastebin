3c7a5be753bc6d172b9dbe1d0806e664f66f1b92
Praveen123-png <praveenkumar814444@gmail.com>
2026-02-02 21:31:36 +0530

    files and UI done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center h-screen w-screen gap-5">
-      <h1 className="text-3xl text-center">Write your message below:</h1>
+      <h1 className="text-3xl px-3 text-center">Write your message below:</h1>
 
       <textarea
         name="paste"
         id="paste"
-        cols={40}
+        cols={30}
         rows={5}
         className="border border-white rounded-lg p-2"
       ></textarea>
 
       <button className="bg-white rounded-lg text-black p-2 hover:bg-gray-200 cursor-pointer transition-color duration-300 active:bg-gray-300">
         Create URL
-      </button> 
+      </button>
 
-      <p>Click the &quot;Click Url&quot; button to create an url for your message</p>
+      <p className="text-center px-3">
+        Click the &quot;Click Url&quot; button to create an url for your message
+      </p>
     </section>
   );
 }
