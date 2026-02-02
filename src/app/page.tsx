ccc3fb735a94bfb94d65ef42095358afc4a03372
Praveen123-png <praveenkumar814444@gmail.com>
2026-02-02 21:13:50 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center h-screen w-screen gap-5">
+      <h1 className="text-3xl text-center">Write your message below:</h1>
+
+      <textarea
+        name="paste"
+        id="paste"
+        cols={40}
+        rows={5}
+        className="border border-white rounded-lg p-2"
+      ></textarea>
+
+      <button className="bg-white rounded-lg text-black p-2 hover:bg-gray-200 cursor-pointer transition-color duration-300 active:bg-gray-300">
+        Create URL
+      </button> 
+
+      <p>Click the &quot;Click Url&quot; button to create an url for your message</p>
+    </section>
+  );
+}
